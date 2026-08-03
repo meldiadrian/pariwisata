694cb705edd4846226cc8e5641c02143b30e58c3
Adrian <154045838+meldiadrian@users.noreply.github.com>
2026-08-03 11:50:24 +0700

    change sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
+{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>{{ url('/') }}</loc>
@@ -6,6 +6,7 @@
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>
+
     @foreach ($news as $item)
     <url>
         <loc>{{ route('news.show', $item->slug) }}</loc>
